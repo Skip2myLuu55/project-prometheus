352f1187883d19105247871a08bced3fc58c46b3
skip2myluu55 <skip2myluu55@gmail.com>
2020-06-23 22:50:14 +0000

    fixed public subnets and centos size

--- prometheus-grafana/provider.tf
+++ prometheus-grafana/provider.tf
@@ -15,6 +15,7 @@ resource "aws_subnet" "public1" {
   vpc_id     = "${aws_vpc.project1.id}"
   availability_zone = "us-east-1a"
   cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = true
   tags = {
     Name = "public1"
   }
@@ -23,6 +24,7 @@ resource "aws_subnet" "public2" {
   vpc_id     = "${aws_vpc.project1.id}"
   availability_zone = "us-east-1b"
   cidr_block = "10.0.2.0/24"
+  map_public_ip_on_launch = true
   tags = {
     Name = "public2"
   }
@@ -31,6 +33,7 @@ resource "aws_subnet" "public3" {
   vpc_id     = "${aws_vpc.project1.id}"
   availability_zone = "us-east-1c"
   cidr_block = "10.0.3.0/24"
+  map_public_ip_on_launch = true
   tags = {
     Name = "public3"
   }
--- prometheus-grafana/provisoner.tf
+++ prometheus-grafana/provisoner.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "Node1" {
   ami           = "${data.aws_ami.centos7.id}"
   key_name      = "${aws_key_pair.deployer.key_name}"
   subnet_id = "${aws_subnet.public1.id}"
-  security_groups  = ["${aws_security_group.pro_graf_security.id}"]
+  vpc_security_group_ids  = ["${aws_security_group.pro_graf_security.id}"]
   instance_type = "t3.micro"
   provisioner   "remote-exec" {
     connection {
@@ -24,7 +24,7 @@ resource "aws_instance" "Node1" {
 }
 
 # resource "aws_instance" "Node2" {
-#   ami           = "${data.aws_ami.centos8.id}"
+#   ami           = "${data.aws_ami.centos7.id}"
 #   key_name      = "${aws_key_pair.deployer.key_name}"
 #   security_groups  = ["${aws_security_group.pro_graf_security.name}"]
 #   instance_type = "t2.micro"
@@ -42,7 +42,7 @@ resource "aws_instance" "Node1" {
 # }
 
 # resource "aws_instance" "Node3" {
-#   ami           = "${data.aws_ami.centos8.id}"
+#   ami           = "${data.aws_ami.centos7.id}"
 #   key_name      = "${aws_key_pair.deployer.key_name}"
 #   security_groups  = ["${aws_security_group.pro_graf_security.name}"]
 #   instance_type = "t2.micro"
