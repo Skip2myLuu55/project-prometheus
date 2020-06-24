57aa7d89b1220d59a82f7a47e08a67a3676e1e69
skip2myluu55 <skip2myluu55@gmail.com>
2020-06-24 05:23:03 +0000

    edits to node

--- prometheus-grafana/null_resource_node.tf
+++ prometheus-grafana/null_resource_node.tf
@@ -3,18 +3,36 @@
         always_run = "${timestamp()}"
     }    
   depends_on = ["aws_instance.node1"]
+  provisioner   "file" {
+    connection {
+        host        = "${aws_instance.node.public_ip}"
+        type        = "ssh"
+        user        = "centos"
+        private_key = "${file("~/.ssh/id_rsa")}"
+    }
+    source  =  "./configuration_files/prometheus_configs/node_exporter.service"
+    destination = "/tmp/node_exporter.service"
+  },
+  depends_on = ["aws_instance.node"]
   provisioner   "remote-exec" {
     connection {
-        host        = "${aws_instance.web.public_ip}"
+        host        = "${aws_instance.node.public_ip}"
         type        = "ssh"
         user        = "centos"
         private_key = "${file("~/.ssh/id_rsa")}"
     }
     inline = [
-    # Installs node exporter
-     "sudo wget -P /tmp https://github.com/prometheus/node_exporter/releases/download/v0.18.1/node_exporter-0.18.1.linux-amd64.tar.gz",
-     "tar xvfz node_exporter-0.18.1.linux-amd64.tar.gz", 
-     "sudo mv node_exporter-0.18.1.linux-amd64 /etc/prometheus/node_exporter" 
+      "sudo mv node_exporter-0.18.1.linux-amd64 /etc/prometheus/node_exporter",
+      # change ownership to prometheus
+      "sudo chown -R prometheus:prometheus /etc/prometheus/node_exporter",
+      # move service file to /etc folder and change ownership to root
+      "sudo mv /tmp/node_exporter.service /etc/systemd/system/node_exporter.service",
+      "sudo chown root:root /etc/systemd/system/node_exporter.service",
+      # start and enable services
+      "sudo systemctl daemon-reload", 
+      "sudo systemctl start node_exporter",
+      "sudo systemctl enable node_exporter",
+      "sudo systemctl status node_exporter"
     ]
   }
 }
